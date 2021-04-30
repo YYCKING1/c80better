0263c0e092befa8b01b71fd1981ee4bd801c1739
YYCKING1 <75652154+YYCKING1@users.noreply.github.com>
2021-04-30 16:06:43 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function addUser()
+{
+    words=document.getElementById("number1","number2","number3","number4","number5","number6","number7","number8").value;
+    localStorage.setItem("number1","number2","number3","number4","number5","number6","number7","number8",word);
+
+     localStorage.getItem("number1","number2","number3","number4","number5","number6","number7","number8");
+}
